85370589e44abb4d7a1334df8e6b7d32360dc7ae
Luis de Bethencourt <luisbg@osg.samsung.com>
2015-12-08 15:19:05 +0000

    add mu4e

--- emacs.el
+++ emacs.el
@@ -473,3 +473,69 @@ This command is convenient when reading novel, documentation."
               (propertize "[Mod]"
                  'face 'font-lock-warning-face
                  'help-echo "Buffer has been modified")))))
+
+;; mu4e config
+(add-to-list 'load-path "/usr/share/emacs24/site-lisp/mu4e")
+(require 'smtpmail)
+
+; smtp
+(setq message-send-mail-function 'smtpmail-send-it
+      smtpmail-starttls-credentials
+      '(("imap.s-osg.org" 587 nil nil))
+      smtpmail-default-smtp-server "imap.s-osg.org"
+      smtpmail-smtp-server "imap.s-osg.org"
+      smtpmail-smtp-service 587
+      smtpmail-debug-info t)
+
+(require 'mu4e)
+
+(setq mu4e-maildir (expand-file-name "~/email/syncmail"))
+
+(setq mu4e-drafts-folder "/Drafts")
+(setq mu4e-sent-folder   "/Sent")
+(setq mu4e-trash-folder  "/Trash")
+(setq message-signature-file "~/.emacs.d/.signature") ; put your signature in this file
+
+; get mail
+(setq mu4e-get-mail-command "mbsync -c ~/.emacs.d/.mbsyncrc osg"
+      mu4e-html2text-command "w3m -T text/html"
+      mu4e-update-interval 120
+      mu4e-headers-auto-update t
+      mu4e-compose-signature-auto-include nil)
+
+(setq mu4e-maildir-shortcuts
+      '( ("/INBOX"   . ?i)
+         ("/Sent"    . ?s)
+         ("/Trash"   . ?t)
+         ("/INBOX/.GStreamerDevel"   . ?g)
+         ("/Drafts"  . ?d)))
+
+;;(eval-after-load 'mu4e
+;;  (progn
+;;    (define-key org-mode-map (kbd "<M-right>") nil)
+;;    (define-key org-mode-map (kbd "<M-left>") nil)
+;;    (define-key org-mode-map (kbd "<M-up>") nil)
+;;    (define-key mu4e-view-headers-next (kbd "<M-down>") nil)))
+
+;; show images
+(setq mu4e-show-images t)
+
+;; use imagemagick, if available
+(when (fboundp 'imagemagick-register-types)
+  (imagemagick-register-types))
+
+;; general emacs mail settings; used when composing e-mail
+;; the non-mu4e-* stuff is inherited from emacs/message-mode
+(setq mu4e-reply-to-address "luisbg@osg.samsung.com"
+    user-mail-address "luisbg@osg.samsung.com"
+    user-full-name  "Luis de Bethencourt")
+
+;; don't save message to Sent Messages, IMAP takes care of this
+; (setq mu4e-sent-messages-behavior 'delete)
+
+;; spell check
+(add-hook 'mu4e-compose-mode-hook
+        (defun my-do-compose-stuff ()
+           "My settings for message composition."
+           (set-fill-column 76)
+           (flyspell-mode)))
