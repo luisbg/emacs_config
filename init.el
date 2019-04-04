6d2e15874df25c768881cef5c5488a2b40304f40
Luis de Bethencourt <luisbg@amazon.com>
2019-04-04 18:00:46 -0400

    swap-windows

--- init.el
+++ init.el
@@ -17,6 +17,8 @@
 (ido-mode 1)
 (setq ido-enable-flex-matching t)
 (setq ido-everywhere t)
+;; (to get out of ido mode and be able to create a new file)
+;; C-x C-f C-f
 
 (set-cursor-color "chocolate")
 (column-number-mode t)
@@ -52,3 +54,38 @@
 (custom-set-variables
     '(eshell-visual-options (quote (("git" "log" "diff" "show"))))
 )
+
+;; Swap right and left windows
+(require 'cl)
+(defun swap-windows ()
+    "If you have 2 windows, it swaps them."
+    (interactive)
+    (cond ((not (= (count-windows) 2))
+        (message "You need exactly 2 windows to do this."))
+        (t
+        (let* ((w1 (first
+                        (window-list)
+                    )
+                )
+                (w2
+                    (second
+                        (window-list)
+                    )
+                )
+                (b1 (window-buffer w1))
+                (b2 (window-buffer w2))
+                (s1 (window-start w1))
+                (s2
+                    (window-start w2)
+                )
+            )
+
+            (set-window-buffer w1 b2)
+            (set-window-buffer w2 b1)
+            (set-window-start w1 s2)
+            (set-window-start w2 s1)
+
+            ; re-select the previously selected buffer
+            (if (eq (selected-window) w1)
+            (select-window w2)
+            (select-window w1))))))
